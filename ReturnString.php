8a5f96a3d4791ab5cdffb28ddf561be538a96ea2
abata15 <abata15@gmail.com>
2016-08-16 12:12:02 -0400

    Add files via upload

---
+++ ReturnString.php
@@ -0,0 +1,22 @@
+<?php
+// Create a string
+
+$sampleString = "I Got The H1B Blues";
+?>
+
+<!DOCTYPE html 	PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+
+<html lang="en">
+	<head>
+		<title>H1B Blues</title>
+	</head>
+	
+	<body>
+	
+	<?php 
+	//Display the string
+		echo $sampleString . "<br />";
+	?>
+	
+	</body>
+</html>
